29e0b0439ef3addda823f7e07606fafc1804b9f5
frank <frank@rothmann-net.de>
2014-03-01 09:58:19 +0100

    v 0.9.8-5

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -3,5 +3,5 @@ class Todo < ActiveRecord::Base
   belongs_to :team
   belongs_to :project
   belongs_to :projectevent
-  attr_accessible :description, :project_id, :todotitle, :user_id,:state, :position, :projectevent_id
+  attr_accessible :description, :project_id, :todotitle, :user_id,:state, :position, :projectevent_id, :projectlink
 end
